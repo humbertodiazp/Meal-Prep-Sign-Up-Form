793113cbc4ccbd9712067c620d4fe98cd424a057
Humberto <hdkaratechop@gmail.com>
2022-10-25 15:51:15 -0700

    add styling for body, add background image to sidebar

---
+++ style.cs
@@ -0,0 +1,8 @@
+body {
+    margin: 0;
+    padding: 0;
+  }
+
+.sidebar {
+    background: url('https://lh5.googleusercontent.com/p/AF1QipOw-u6JiKzOgc7EIsvm_6dsrTgpHrFkXkOIDfgJ=s1354-k-no') right;
+}
