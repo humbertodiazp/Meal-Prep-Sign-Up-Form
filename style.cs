3fba34e30221f438afae7cd740f39dbbc0fc1de5
Humberto <hdkaratechop@gmail.com>
2022-10-25 16:44:24 -0700

    remove style c file

--- style.cs
+++
@@ -1,8 +0,0 @@
-body {
-    margin: 0;
-    padding: 0;
-  }
-
-.sidebar {
-    background: url('https://lh5.googleusercontent.com/p/AF1QipOw-u6JiKzOgc7EIsvm_6dsrTgpHrFkXkOIDfgJ=s1354-k-no') right;
-}
